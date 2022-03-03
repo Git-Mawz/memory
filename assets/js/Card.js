456ba93082e90273d138ac0bdc05cf5880cf9c06
Git-Mawz <py.oclock@gmail.com>
2022-03-03 16:17:58 +0100

    correction d'indentation, ajout d'event sur les cartes, creation des propriétés css pour les cartes

--- assets/js/Card.js
+++ assets/js/Card.js
@@ -9,12 +9,25 @@ class Card {
         this.value = value;
     }
 
+    /**
+     * Créé l'élément correspondant à la carte en vu de l'ajouter au jeu
+     */
     getElement() {
         this.element = document.createElement('div');
         this.element.classList.add('card');
         this.element.classList.add('card--' + this.value);
+
+        // On ajoute un event listener sur la carte
+        this.element.addEventListener('click', event => {this.handleClick(event)});
       
         return this.element;
+    }
+
+    /**
+     * Gère l'event de clock sur la carte
+     */
+    handleClick() {
+        console.log(this.element);
     }
 
 }
--- assets/js/Game.js
+++ assets/js/Game.js
@@ -13,15 +13,15 @@ class Game
         this.containerElement = document.getElementById(selector);
         // On créé un tableau avec les pair de chiffres
         for(let i = 0 ; i < this.pairNumber ; i++) {
-          this.matrix.push(i);
-          this.matrix.push(i);
+            this.matrix.push(i);
+            this.matrix.push(i);
         }
         // On mélange les cartes
         this.matrix = this.shuffle(this.matrix);
 
         // On créé les cartes et on les garde dans le jeu
         this.matrix.forEach(value => {
-          this.cards.push(new Card(this, value));
+            this.cards.push(new Card(this, value));
         });
     }
 
@@ -30,8 +30,8 @@ class Game
      */
     render() {
         this.cards.forEach(card => {
-          const cardElement = card.getElement();
-          this.containerElement.appendChild(cardElement);
+            const cardElement = card.getElement();
+            this.containerElement.appendChild(cardElement);
         });
     }
 
@@ -43,11 +43,11 @@ class Game
     shuffle(array) {
         let length = array.length;
         while (length) {
-          length--;
-          const randomIndex = Math.floor(Math.random() * length);
-          const savedValue = array[length];
-          array[length] = array[randomIndex];
-          array[randomIndex] = savedValue;
+            length--;
+            const randomIndex = Math.floor(Math.random() * length);
+            const savedValue = array[length];
+            array[length] = array[randomIndex];
+            array[randomIndex] = savedValue;
         }
         return array;
     }
